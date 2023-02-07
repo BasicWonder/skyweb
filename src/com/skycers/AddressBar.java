5546ece244b6365e31c22dd3322b40ec9ccbe9d2
BasicWonder <41087851+BasicWonder@users.noreply.github.com>
2023-02-07 11:26:42 -0500

    Create AddressBar.java

---
+++ src/com/skycers/AddressBar.java
@@ -0,0 +1,35 @@
+package com.skycers;
+import javafx.scene.control.*;
+import javafx.event.*;
+import javafx.scene.input.*;
+
+public class AddressBar extends TextField{
+    public AddressBar(TabManager manager){
+        
+         
+        TextField self = this;
+        
+        this.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                if(event.getCode().equals(KeyCode.ENTER)) {
+                    boolean resultFound = false;
+                    String easyUrl = self.getText();
+                    for (Sites site : Sites.values()) { 
+                        if(!resultFound){
+                            boolean tip = site.match(self.getText());
+                            System.out.println(tip);
+                        if(tip){
+                            resultFound = true;
+                            easyUrl = site.url();
+                        }
+                        }
+                    }
+                    
+                    
+                     manager.set(easyUrl);
+                }
+            }
+        });
+    }
+}
