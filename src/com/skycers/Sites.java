bb082b66a283291929b47b3f4f637ba5b857e3e1
BasicWonder <41087851+BasicWonder@users.noreply.github.com>
2023-02-09 10:49:39 -0500

    Update Sites.java

--- src/com/skycers/Sites.java
+++ src/com/skycers/Sites.java
@@ -2,7 +2,13 @@ package com.skycers;
 enum Sites{
    GOOGLE,
    YOUTUBE,
-   YAHOO;
+   YAHOO,
+   DISCORD,
+   INSTAGRAM,
+   TWITTER,
+   EBAY,
+   QUORA;
+
    
    
       public boolean match(String compareTo) {
@@ -26,6 +32,16 @@ enum Sites{
                     return new String[] {"yt","youtube","you","yatube","yotube","youtub","you tube"};
                 case YAHOO: 
                     return new String[] {"yah","yahoo","yaho","yhoo","yaooh"};
+                case DISCORD: 
+                    return new String[] {"discord","discor","disco","dis"};
+                case TWITTER: 
+                    return new String[] {"twitt","twitter","tweet"};
+                case INSTAGRAM: 
+                    return new String[] {"insta","instagram","gram","instagra"};
+                case EBAY: 
+                    return new String[] {"ebay","e-bay","eba","elay"};
+                case QUORA: 
+                    return new String[] {"quora","quroa","quo","qur"};
                 default: 
                     return null;
             }
@@ -40,6 +56,16 @@ enum Sites{
                     return "https://youtube.com";
                 case YAHOO: 
                     return "https://yahoo.com";
+                case DISCORD: 
+                    return "https://discord.com";
+                case TWITTER: 
+                    return "https://twitter.com";
+                case INSTAGRAM: 
+                    return "https://instagram.com";
+                case EBAY: 
+                    return "https://ebay.com";
+                case QUORA: 
+                    return "https://quora.com";
                 default: 
                     return null;
             }
