8338bf65a4431b2a24dc9a0f71374796afa4e10f
BasicWonder <41087851+BasicWonder@users.noreply.github.com>
2023-02-09 10:46:47 -0500

    Update Package.java

--- src/Package.java
+++ src/Package.java
@@ -2,13 +2,15 @@ import java.util.*;
 
 public class Package{
     public String TITLE = "Skyweb Beta Client";
-    public String VERSION = "beta-candidate-0.1";
-    public String TYPE = "exp";
-    public String UPDATED = "2023-02-07";
+    public String VERSION = "beta-1.0";
+    public String TYPE = "rel";
+    public String UPDATED = "2023-02-09";
     
     public Package(){
         
-        System.out.println(this.TITLE + ": " + this.VERSION + " (" + UPDATED + ")" + " " + TYPE);
+        System.out.println(this.TITLE + ": " + this.VERSION + " (" + this.UPDATED + ")" + " " + this.TYPE);
+        
+        
     }
     
     
